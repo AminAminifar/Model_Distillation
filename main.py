35fa25385ed76fe5a4047da6110b48d62726f79d
Amin Aminifar <43568226+AminAminifar@users.noreply.github.com>
2023-11-05 16:53:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 # Stop showing warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 from tensorflow import keras
-import tensorflow as tf
-from keras import layers
 import numpy as np
+
+import models
+import Distiller
 
 
 ################################################################
@@ -14,192 +14,28 @@ import numpy as np
 # https://github.com/keras-team/keras-io/blob/master/examples/vision/knowledge_distillation.py
 ################################################################
 
-################################################################
-# Construct Distiller() class
-################################################################
+# set parameters
+# batch_size = 15
+model_num_teacher = 0  # check models file for details
+model_num_student = 0  # check models file for details
 
-class Distiller(keras.Model):
-    def __init__(self, student, teacher):
-        super().__init__()
-        self.teacher = teacher
-        self.student = student
+batch_size_teacher = 100
+epochs_teacher = 5
 
-    def compile(self, optimizer, metrics, student_loss_fn, distillation_loss_fn, alpha=0.1, temperature=3,):
-        """ Configure the distiller.
+temperature = 20
+alpha = 0.1
+batch_size_student = 100
+epochs_student = 5
 
-        Args:
-            optimizer: Keras optimizer for the student weights
-            metrics: Keras metrics for evaluation
-            student_loss_fn: Loss function of difference between student
-                predictions and ground-truth
-            distillation_loss_fn: Loss function of difference between soft
-                student predictions and soft teacher predictions
-            alpha: weight to student_loss_fn and 1-alpha to distillation_loss_fn
-            temperature: Temperature for softening probability distributions.
-                Larger temperature gives softer distributions.
-        """
-        super().compile(optimizer=optimizer, metrics=metrics)
-        self.student_loss_fn = student_loss_fn
-        self.distillation_loss_fn = distillation_loss_fn
-        self.alpha = alpha
-        self.temperature = temperature
-
-    def train_step(self, data):
-        # Unpack data
-        x, y = data
-
-        # Forward pass of teacher
-        teacher_predictions = self.teacher(x, training=False)
-
-        with tf.GradientTape() as tape:
-            # Forward pass of student
-            student_predictions = self.student(x, training=True)
-
-            # Compute losses
-            student_loss = self.student_loss_fn(y, student_predictions)
-
-            # Compute scaled distillation loss from https://arxiv.org/abs/1503.02531
-            # The magnitudes of the gradients produced by the soft targets scale
-            # as 1/T^2, multiply them by T^2 when using both hard and soft targets.
-            distillation_loss = (
-                self.distillation_loss_fn(
-                    tf.nn.softmax(teacher_predictions / self.temperature, axis=1),
-                    tf.nn.softmax(student_predictions / self.temperature, axis=1),
-                )
-                * self.temperature**2
-            )
-
-            loss = self.alpha * student_loss + (1 - self.alpha) * distillation_loss
-
-        # Compute gradients
-        trainable_vars = self.student.trainable_variables
-        gradients = tape.gradient(loss, trainable_vars)
-
-        # Update weights
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-
-        # Update the metrics configured in `compile()`.
-        self.compiled_metrics.update_state(y, student_predictions)
-
-        # Return a dict of performance
-        results = {m.name: m.result() for m in self.metrics}
-        results.update(
-            {"student_loss": student_loss, "distillation_loss": distillation_loss}
-        )
-        return results
-
-    def test_step(self, data):
-        # Unpack the data
-        x, y = data
-
-        # Compute predictions
-        y_prediction = self.student(x, training=False)
-
-        # Calculate the loss
-        student_loss = self.student_loss_fn(y, y_prediction)
-
-        # Update the metrics.
-        self.compiled_metrics.update_state(y, y_prediction)
-
-        # Return a dict of performance
-        results = {m.name: m.result() for m in self.metrics}
-        results.update({"student_loss": student_loss})
-        return results
-
+check_student_scratch_flag = True
+batch_size_student_scratch = 100
+epochs__student_scratch = 5
 ################################################################
 # Create student and teacher models
 ################################################################
 
-# Create the teacher
-# teacher = keras.Sequential(
-#     [
-#         keras.Input(shape=(28, 28, 1)),
-#         layers.Conv2D(256, (3, 3), strides=(2, 2), padding="same"),
-#         layers.LeakyReLU(alpha=0.2),
-#         layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding="same"),
-#         layers.Conv2D(512, (3, 3), strides=(2, 2), padding="same"),
-#         layers.Flatten(),
-#         layers.Dense(10),
-#     ],
-#     name="teacher",
-# )
-
-teacher = keras.Sequential(
-    [
-        tf.keras.layers.InputLayer(input_shape=[28, 28, 1]),
-
-        # # first hidden layer
-        # tf.keras.layers.Conv2D(filters=800, kernel_size=3, strides=1, padding="same"),  # , input_shape=(28, 28, 1)
-        # tf.keras.layers.BatchNormalization(),
-        # tf.keras.layers.Activation('relu'),
-        # tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
-        #
-        # # second hidden layer
-        # tf.keras.layers.Conv2D(filters=800, kernel_size=3, strides=1, padding="same"),
-        # tf.keras.layers.BatchNormalization(),
-        # tf.keras.layers.Activation('relu'),
-        # tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
-
-        # third hidden layer
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=800),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Activation('relu'),
-
-        tf.keras.layers.Dense(units=800),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Activation('relu'),
-
-        # output layer
-        tf.keras.layers.Dense(units=10, activation=tf.nn.softmax)
-    ],
-    name="teacher",
-)
-
-# Create the student
-# student = keras.Sequential(
-#     [
-#         keras.Input(shape=(28, 28, 1)),
-#         layers.Conv2D(16, (3, 3), strides=(2, 2), padding="same"),
-#         layers.LeakyReLU(alpha=0.2),
-#         layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding="same"),
-#         layers.Conv2D(32, (3, 3), strides=(2, 2), padding="same"),
-#         layers.Flatten(),
-#         layers.Dense(10),
-#     ],
-#     name="student",
-# )
-
-student = keras.Sequential(
-    [
-        keras.Input(shape=(28, 28, 1)),
-        # tf.keras.layers.Conv2D(filters=300, kernel_size=3, strides=1, padding="same"),
-        # tf.keras.layers.BatchNormalization(),
-        # tf.keras.layers.Activation('relu'),
-        # tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
-        #
-        #
-        # tf.keras.layers.Conv2D(filters=300, kernel_size=3, strides=1, padding="same"),
-        # tf.keras.layers.BatchNormalization(),
-        # tf.keras.layers.Activation('relu'),
-        # tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
-
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=300),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Activation('relu'),
-
-        tf.keras.layers.Dense(units=300),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Activation('relu'),
-
-        # layers.Flatten(),
-        layers.Dense(10),
-    ],
-    name="student",
-)
-
-
+teacher = models.import_teacher(model_num=model_num_teacher)
+student = models.import_student(model_num=model_num_student)
 # Clone student for later comparison
 student_scratch = keras.models.clone_model(student)
 
@@ -207,8 +43,8 @@ student_scratch = keras.models.clone_model(student)
 # Prepare the dataset
 ################################################################
 # Prepare the train and test dataset.
-batch_size = 64
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
 
 # Normalize data
 x_train = x_train.astype("float32") / 255.0
@@ -216,8 +52,6 @@ x_train = np.reshape(x_train, (-1, 28, 28, 1))
 
 x_test = x_test.astype("float32") / 255.0
 x_test = np.reshape(x_test, (-1, 28, 28, 1))
-
-
 ################################################################
 # Train the teacher
 ################################################################
@@ -232,7 +66,8 @@ teacher.compile(
 )
 
 # Train and evaluate teacher on data.
-teacher.fit(x_train, y_train, epochs=20)
+teacher.fit(x_train, y_train, batch_size=batch_size_teacher, epochs=epochs_teacher)
+print("evaluate teacher based on x_test, y_test: ")
 teacher.evaluate(x_test, y_test)
 
 ################################################################
@@ -242,44 +77,39 @@ print("################################################################")
 print("Train and evaluate the distilled student model")
 print("################################################################")
 # Initialize and compile distiller
-distiller = Distiller(student=student, teacher=teacher)
-# distiller.compile(
-#     optimizer=keras.optimizers.Adam(),
-#     metrics=[keras.metrics.SparseCategoricalAccuracy()],
-#     student_loss_fn=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#     distillation_loss_fn=keras.losses.KLDivergence(),
-#     alpha=0.1,
-#     temperature=10,
-# )
+distiller = Distiller.Distiller(student=student, teacher=teacher)
 
 distiller.compile(
     optimizer=keras.optimizers.Adam(),
     metrics=[keras.metrics.SparseCategoricalAccuracy()],
     student_loss_fn=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     distillation_loss_fn=keras.losses.KLDivergence(),
-    alpha=0.1,
-    temperature=20,
+    alpha=alpha,
+    temperature=temperature,
 )
 
 # Distill teacher to student
-distiller.fit(x_train, y_train, epochs=20)
+distiller.fit(x_train, y_train, batch_size=batch_size_student, epochs=epochs_student)  # x_train, y_train, x_train_, y_train_, batch_size=64
 
 # Evaluate student on test dataset
+print("evaluate student based on x_test, y_test: ")
 distiller.evaluate(x_test, y_test)
 
 ################################################################
 # Train student from scratch for comparison
 ################################################################
-print("################################################################")
-print("Train and evaluate the (non distilled) student model for comparison")
-print("################################################################")
-# Train student as doen usually
-student_scratch.compile(
-    optimizer=keras.optimizers.Adam(),
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=[keras.metrics.SparseCategoricalAccuracy()],
-)
+if check_student_scratch_flag:
+    print("################################################################")
+    print("Train and evaluate the (non distilled) student model for comparison")
+    print("################################################################")
+    # Train student as doen usually
+    student_scratch.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    )
 
-# Train and evaluate student trained from scratch.
-student_scratch.fit(x_train, y_train, epochs=20)
-student_scratch.evaluate(x_test, y_test)
+    # Train and evaluate student trained from scratch.
+    student_scratch.fit(x_train, y_train, batch_size=batch_size_student_scratch, epochs=epochs__student_scratch)
+    print("evaluate student_scratch based on x_test, y_test: ")
+    student_scratch.evaluate(x_test, y_test)
---
+++ models.py
@@ -0,0 +1,137 @@
+from tensorflow import keras
+from keras import layers
+import tensorflow as tf
+
+
+def import_teacher(model_num=0):
+    if model_num == 0:
+        # Create the teacher
+        teacher = keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=[28, 28, 1]),
+
+
+                # third hidden layer
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(units=800),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+
+                tf.keras.layers.Dense(units=800),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+
+                # output layer
+                tf.keras.layers.Dense(units=10, activation=tf.nn.softmax)
+            ],
+            name="teacher",
+        )
+
+    if model_num == 1:
+        # Create the teacher
+        teacher = keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=[28, 28, 1]),
+
+                # first hidden layer
+                tf.keras.layers.Conv2D(filters=100, kernel_size=3, strides=1, padding="same"),  # , input_shape=(28, 28, 1)
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+                tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+                # second hidden layer
+                tf.keras.layers.Conv2D(filters=100, kernel_size=3, strides=1, padding="same"),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+                tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+                # third hidden layer
+                tf.keras.layers.Flatten(),
+
+                # output layer
+                tf.keras.layers.Dense(units=10, activation=tf.nn.softmax)
+            ],
+            name="teacher",
+        )
+
+    if model_num == 2:
+        # Create the teacher
+        teacher = keras.Sequential(
+            [
+                keras.Input(shape=(28, 28, 1)),
+                layers.Conv2D(256, (3, 3), strides=(2, 2), padding="same"),
+                layers.LeakyReLU(alpha=0.2),
+                layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding="same"),
+                layers.Conv2D(512, (3, 3), strides=(2, 2), padding="same"),
+                layers.Flatten(),
+                layers.Dense(10),
+            ],
+            name="teacher",
+        )
+
+    return teacher
+
+
+def import_student(model_num=0):
+
+    if model_num == 0:
+        # Create the student
+        student = keras.Sequential(
+            [
+                keras.Input(shape=(28, 28, 1)),
+
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(units=300),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+
+                tf.keras.layers.Dense(units=300),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+
+                # layers.Flatten(),
+                layers.Dense(10),
+            ],
+            name="student",
+        )
+
+    if model_num == 1:
+        # Create the student
+        student = keras.Sequential(
+            [
+                keras.Input(shape=(28, 28, 1)),
+                tf.keras.layers.Conv2D(filters=50, kernel_size=3, strides=1, padding="same"),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+                tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+
+                tf.keras.layers.Conv2D(filters=50, kernel_size=3, strides=1, padding="same"),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Activation('relu'),
+                tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+                tf.keras.layers.Flatten(),
+
+                # layers.Flatten(),
+                layers.Dense(10),
+            ],
+            name="student",
+        )
+
+    if model_num == 2:
+    # Create the student
+        student = keras.Sequential(
+            [
+                keras.Input(shape=(28, 28, 1)),
+                layers.Conv2D(16, (3, 3), strides=(2, 2), padding="same"),
+                layers.LeakyReLU(alpha=0.2),
+                layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding="same"),
+                layers.Conv2D(32, (3, 3), strides=(2, 2), padding="same"),
+                layers.Flatten(),
+                layers.Dense(10),
+            ],
+            name="student",
+        )
+
+    return student
